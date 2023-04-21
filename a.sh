2dd3bb6df1cbab562072f2d632a5d8e8ad66c3db
DaoKhanhDuy <90609070+DaoKhanhDuy@users.noreply.github.com>
2023-04-21 20:19:42 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,29 +1,55 @@
 #!/bin/bash
 
-# Hiển thị menu
-echo "1. Git clone từ GitHub"
-echo "2. Cài đặt các package"
-echo "3. Hiển thị các thư mục đã git"
-
-# Đọc lựa chọn từ người dùng
-read choice
-
-# Xử lý lựa chọn của người dùng
-case $choice in
-  1)
-    echo "Bạn đã chọn Git clone từ GitHub"
-    # Thực hiện các lệnh để git clone từ GitHub
-    ;;
-  2)
-    echo "Bạn đã chọn cài đặt các package"
-    # Thực hiện các lệnh để cài đặt các package
-    ;;
-  3)
-    echo "Bạn đã chọn hiển thị các thư mục đã git"
-    # Thực hiện các lệnh để hiển thị các thư mục đã git
-    ;;
-  *)
-    echo "Lựa chọn không hợp lệ. Vui lòng chọn lại."
-    # Thực hiện các lệnh để thông báo lựa chọn không hợp lệ
-    ;;
-esac
+PS3='Vui lòng chọn một trong các lựa chọn bên dưới:'
+options=("Git clone và đăng nhập" "Cài đặt package" "Hiển thị danh sách các thư mục" "Thoát")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Git clone và đăng nhập")
+            echo "Bạn đã chọn Git clone và đăng nhập"
+            read -p "Vui lòng nhập link Github: " github_link
+            if [[ $github_link == *"github.com"* ]]
+            then
+                git clone $github_link
+                echo "Clone thành công!"
+            else
+                echo "Link Github không hợp lệ"
+            fi
+            ;;
+        "Cài đặt package")
+            echo "Bạn đã chọn cài đặt package"
+            read -p "Vui lòng nhập đường dẫn đến thư mục cần cài đặt: " package_path
+            cd $package_path
+            npm install
+            echo "Cài đặt thành công!"
+            ;;
+        "Hiển thị danh sách các thư mục")
+            echo "Bạn đã chọn hiển thị danh sách các thư mục"
+            folders=($(find . -maxdepth 1 -type d))
+            for i in "${!folders[@]}"; do
+                printf "%s\t%s\n" "$i" "${folders[$i]}"
+            done
+            read -p "Vui lòng nhập số thư mục cần truy cập: " folder_index
+            if [[ "$folder_index" =~ ^[0-9]+$ ]]
+            then
+                selected_folder="${folders[$folder_index]}"
+                if [[ -d $selected_folder ]]
+                then
+                    cd $selected_folder
+                    echo "Bạn đã truy cập vào thư mục $selected_folder"
+                else
+                    echo "Số thư mục không hợp lệ"
+                fi
+            else
+                echo "Số thư mục không hợp lệ"
+            fi
+            ;;
+        "Thoát")
+            echo "Bạn đã chọn thoát"
+            break
+            ;;
+        *) 
+            echo "Lựa chọn không hợp lệ. Hãy chọn lại."
+            ;;
+    esac
+done
