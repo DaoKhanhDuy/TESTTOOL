72ad69d5018a7d8cafa0a1c584d0dc84f2080b9f
DaoKhanhDuy <90609070+DaoKhanhDuy@users.noreply.github.com>
2023-04-22 06:34:27 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,19 @@
-#!/bin/bash
-
-# Cài đặt Ubuntu trên Termux
-curl -sLO https://raw.githubusercontent.com/Neo-Oli/termux-ubuntu/master/ubuntu.sh && chmod +x ubuntu.sh && ./ubuntu.sh
-
-# Cài đặt NodeJS v16, PHP và Wget
-curl -sL https://deb.nodesource.com/setup_16.x | bash -
-pkg install nodejs php wget
+clear
+rm ./install.sh
+echo + Setup storage:
+echo 
+echo y|termux-setup-storage 
+echo 
+echo + Start update package: 
+echo 
+pkg upgrade
+echo
+echo + Install PHP
+echo
+pkg install php -y
+echo
+echo + Install Wget
+echo
+pkg install wget -y
+echo 
+echo +Fetch install file
