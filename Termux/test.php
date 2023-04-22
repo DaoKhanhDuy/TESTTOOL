28549f8f04f451332dbf0b1e6a01fe510082d8e6
DaoKhanhDuy <90609070+DaoKhanhDuy@users.noreply.github.com>
2023-04-22 23:39:06 +0700

    Update test.php

--- Termux/test.php
+++ Termux/test.php
@@ -1,43 +1,18 @@
 <?php
-@system("clear");
-//cp -r /storage/0000-0000/Termux/* ~
-//colors
-$red = "\033[1;31m";
-$green = "\033[1;32m";
-$yellow = "\033[1;33m";
-$blue = "\033[1;34m";
-$res = "\033[1;35m";
-$nau = "\033[1;36m";
-$while = "\033[1;37m";
-//program
-$ggid = "1-9v-_3Ye9SXAMvvTvlMSi-NzuQl6vfAp";
-$link = "https://raw.githubusercontent.com/DaoKhanhDuy/TestTool/main/Termux/install.php";
-$colorl = "https://raw.githubusercontent.com/DaoKhanhDuy/TestTool/main/color.php";
-if (file_exists("install.php") == false) {
-	$content = file_get_contents("../usr/etc/bash.bashrc");
-	if(!strpos($content, "php test.php"))
-		file_put_contents("../usr/etc/bash.bashrc", "\necho \"".$yellow."Type ./start-ubuntu.sh to launch the Ubuntu\"", FILE_APPEND);
-	echo $red,
-	("Start the installer download...\n");
-	//sleep(2);
-	echo $nau,
-	("-------------------------\n"),
-	$green;
-	@system("curl ".$link." --output install.php", $exitcode);
-	@system("curl ".$colorl." --output color.php", $exitcode);
-	//echo $exitcode;
-} else {
-	echo $red,
-	("Start the installer update...\n");
-	echo $nau,
-	("-------------------------\n"),
-	$green;
-	//sleep(2);
-	@system("rm ./install.php && curl ".$link." --output install.php", $exitcode);
-	@system("curl ".$colorl." --output color.php", $exitcode);
-	//echo $exitcode;
-};
-if ($exitcode != 0)
-	echo $nau, ("-------------------------\n").$red."Can't connect to Github.com. Please check the internet and try again late!\n";
-else include("./install.php");
+// Kiểm tra xem Termux đã được cài đặt hay chưa
+if (!is_dir("/data/data/com.termux/files")) {
+    echo "Termux is not installed. Please install Termux and try again.\n";
+    exit();
+}
+
+// Cài đặt các gói cần thiết
+system('pkg update && pkg upgrade -y && pkg install -y proot wget curl');
+
+// Tạo thư mục và tải bash script của Ubuntu từ GitHub
+system('mkdir ~/ubuntu && cd ~/ubuntu && curl https://raw.githubusercontent.com/Neo-Oli/termux-ubuntu/master/ubuntu.sh > ubuntu.sh');
+
+// Chạy bash script của Ubuntu và cài đặt Node.js và Git
+system('cd ~/ubuntu && bash ubuntu.sh && sudo apt update && sudo apt install -y nodejs git');
+
+echo "Ubuntu, Node.js, and Git have been installed successfully.\n";
 ?>
