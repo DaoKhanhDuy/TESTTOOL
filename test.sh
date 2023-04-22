d1f88c4adf308888a4bd9d41d03a990fe0e46e4b
DaoKhanhDuy <90609070+DaoKhanhDuy@users.noreply.github.com>
2023-04-22 23:48:36 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,18 @@
-#!/bin/bash
+# Kiểm tra xem môi trường Termux đã cập nhật hay chưa
+pkg update && pkg upgrade -y
 
-# Install Ubuntu
-curl -fsSL https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh | bash
+# Cài đặt gói cần thiết để cài Ubuntu
+pkg install proot wget -y
+
+# Tải và cài đặt Ubuntu
+cd ~ && mkdir ubuntu && cd ubuntu
+wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Ubuntu20/ubuntu20.sh -O ubuntu.sh
+chmod +x ubuntu.sh
+./ubuntu.sh
+
+# Truy cập vào Ubuntu và cài đặt Node.js v16 và Git
 ./start-ubuntu.sh
-
-# Install Node.js and Git
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt-get install -y nodejs git
+apt-get update
+apt-get install curl -y
+curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
+apt-get install -y nodejs git
