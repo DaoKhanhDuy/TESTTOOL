5d18c7849982d15c7445e91cc1dcc851bfc16045
DaoKhanhDuy <90609070+DaoKhanhDuy@users.noreply.github.com>
2023-04-23 09:00:08 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,3 @@
-# Kiểm tra xem môi trường Termux đã cập nhật hay chưa
-pkg update && pkg upgrade -y
-
 # Cài đặt gói cần thiết để cài Ubuntu
 pkg install proot wget -y
 
@@ -16,6 +13,12 @@ apt-get update && apt-get upgrade -y
 apt-get install curl -y
 curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
 apt-get install -y nodejs git
+
+# Tải và cài đặt Ubuntu
+cd ~ && mkdir ubuntu && cd ubuntu
+wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Ubuntu20/ubuntu20.sh -O ubuntu.sh
+chmod +x ubuntu.sh
+./ubuntu.sh
 
 # Tắt Ubuntu và khởi động lại Termux
 exit
