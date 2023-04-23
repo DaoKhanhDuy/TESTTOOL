40f5969b9ff1aa42c8c57ec59b906e553423fb78
DaoKhanhDuy <90609070+DaoKhanhDuy@users.noreply.github.com>
2023-04-23 09:32:32 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,13 +1,9 @@
-# Kiểm tra xem môi trường Termux đã cập nhật hay chưa
-pkg update && pkg upgrade -y -y -y -y -y -y -y
-
 # Cài đặt gói cần thiết để cài Ubuntu
 pkg install proot wget -y
 
 # Tải và cài đặt Ubuntu
 cd ~ && mkdir ubuntu && cd ubuntu
 wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Ubuntu20/ubuntu20.sh -O ubuntu.sh
-cd ubuntu
 chmod +x ubuntu.sh
 ./ubuntu.sh
 
