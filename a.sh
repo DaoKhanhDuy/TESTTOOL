0306d8b9287b6f94186bfe6b5645c150f024ad47
DaoKhanhDuy <90609070+DaoKhanhDuy@users.noreply.github.com>
2023-04-23 11:33:06 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -4,11 +4,7 @@ pkg install proot -y
 pkg install wget -y
 
 # Tải và cài đặt Ubuntu
-cd ~ && mkdir ubuntu && cd ubuntu
-wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Ubuntu20/ubuntu20.sh -O ubuntu.sh
-chmod +x ubuntu.sh
-./ubuntu.sh
-
+cd ~ && mkdir ubuntu && cd ubuntu && wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Ubuntu20/ubuntu20.sh -O ubuntu.sh && chmod +x ubuntu.sh && ./ubuntu.sh
 # Truy cập vào Ubuntu và cài đặt Node.js v16 và Git
 ./start-ubuntu.sh
 apt-get update && apt-get upgrade -y
